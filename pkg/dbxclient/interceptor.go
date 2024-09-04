5c3b042f79a1335d93436669efbe0930cc0bae4e
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-09-04 10:15:02 -0600

    remove log of header (#7)
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- pkg/dbxclient/interceptor.go
+++ pkg/dbxclient/interceptor.go
@@ -40,7 +40,6 @@ func WithAuthorization(accessToken string, session string) clientv2.RequestInter
 // WithLoggingInterceptor adds a http debug logging interceptor
 func WithLoggingInterceptor() clientv2.RequestInterceptor {
 	return func(ctx context.Context, req *http.Request, gqlInfo *clientv2.GQLRequestInfo, res interface{}, next clientv2.RequestInterceptorFunc) error {
-		fmt.Println("Request header sent:", req.Header)
 		fmt.Println("Request body sent:", req.Body)
 
 		return next(ctx, req, gqlInfo, res)
