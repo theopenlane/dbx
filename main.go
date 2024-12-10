da4fd32946c615f08898c053f79fd8a995b6aebf
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-12-10 11:23:19 -0700

    upgrade deps, fix for breaking changes, fix docker build (#104)
    * upgrade deps, fix for breaking changes, fix docker build
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * limit jobs on renovate changes
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * fix go builds
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * typo
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * cli artifact
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,6 +2,9 @@
 package main
 
 import (
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+	_ "modernc.org/sqlite"
+
 	"github.com/theopenlane/dbx/cmd"
 	_ "github.com/theopenlane/dbx/internal/ent/generated/runtime"
 )
